e46bf8158931a8490a42d2b5faf371168e3e4cbd
Vasyl <vazhovnir@gmail.com>
2020-02-13 13:31:00 +0200

    delete wrong sql script

--- sql-scripts/create database user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'librarydb'@'localhost' IDENTIFIED BY 'librarydb';
-
-GRANT ALL PRIVILEGES ON * . * TO 'librarydb'@'localhost';
-
-ALTER USER 'librarydb'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
